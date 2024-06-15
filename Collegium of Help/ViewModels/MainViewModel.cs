2eaa65de8eddbf75c0976d6f02307870c9bd5583
kicerKs <osk.ch18@gmail.com>
2024-06-15 12:23:01 +0200

    Change view changing to use MVVM command style

--- Collegium of Help/ViewModels/MainViewModel.cs
+++ Collegium of Help/ViewModels/MainViewModel.cs
@@ -1,31 +1,30 @@
-﻿using ReactiveUI;
+﻿using Avalonia.Collections;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Reactive;
 
 namespace Collegium_of_Help.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
     private ViewModelBase _contentViewModel;
+    public AvaloniaDictionary<string, ViewModelBase> views = new AvaloniaDictionary<string, ViewModelBase>();
+    public ReactiveCommand<string, Unit> ChangeView { get; }
     public MainViewModel() 
-    { 
-        Home = new HomeViewModel();
-        Characters = new CharactersViewModel();
-        _contentViewModel = Home;
+    {
+        views.Add("Home", new HomeViewModel());
+        views.Add("Characters", new CharactersViewModel());
+        _contentViewModel = views["Home"];
+        ChangeView = ReactiveCommand.Create<string>(name =>
+                {
+                    ContentViewModel = views[name];
+                });
     }
 
-    public HomeViewModel Home { get; set; }
-    public CharactersViewModel Characters { get; set; }
     public ViewModelBase ContentViewModel
     {
         get => _contentViewModel;
         private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
-    }
-
-    public void ChangeToHome()
-    {
-        ContentViewModel = new HomeViewModel();
-    }
-    public void ChangeToCharacters()
-    {
-        ContentViewModel = new CharactersViewModel();
     }
 }
