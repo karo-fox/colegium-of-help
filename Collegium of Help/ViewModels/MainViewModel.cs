96864f9733340239aeafd27bdea036c637150efb
Karolina <k4aro0@gmail.com>
2024-06-20 22:01:23 +0200

    remove comments

--- Collegium of Help/ViewModels/MainViewModel.cs
+++ Collegium of Help/ViewModels/MainViewModel.cs
@@ -8,23 +8,5 @@ namespace Collegium_of_Help.ViewModels;
 
 public class MainViewModel : ViewModelBase
 {
-    //private ViewModelBase _contentViewModel;
-    //public AvaloniaDictionary<string, ViewModelBase> views = new AvaloniaDictionary<string, ViewModelBase>();
-    //public ReactiveCommand<string, Unit> ChangeView { get; }
-    public MainViewModel() 
-    {
-        //views.Add("Home", new HomeViewModel());
-        //views.Add("Characters", new CharactersViewModel());
-        //_contentViewModel = views["Home"];
-        //ChangeView = ReactiveCommand.Create<string>(name =>
-        //        {
-        //            ContentViewModel = views[name];
-        //        });
-    }
-
-    //public ViewModelBase ContentViewModel
-    //{
-    //    get => _contentViewModel;
-    //    private set => this.RaiseAndSetIfChanged(ref _contentViewModel, value);
-    //}
+    public MainViewModel() {}
 }
